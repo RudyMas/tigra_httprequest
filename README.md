f58a5809a4649c97d7f9460149d3e6c6b6b3b2a4
Rudy Mas <rudy.mas@rudymas.be>
2022-05-25 10:43:49 +0200

    Base code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# tiger_httprequest
-Tiger's HttpRequest
+# PHP Class HttpRequest for the Tiger Framework
+
+This is the Class HttpRequest which is used by the Tiger framework.  
+Can also be used as a standalone plugin for your own code!
+
+## Installation
+Install the package using composer.
+```
+composer require tiger/httprequest
+```
+
+or by editing composer.json yourself and adding:
+```
+"require": {
+    "tiger/httprequest": "*"
+}
+```
---
+++ src/HttpRequest.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Tiger;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class HttpRequest (PHP version 7.4)
+ *
+ * @author Rudy Mas <rudy.mas@rmsoft.be>
+ * @copyright 2022, rmsoft.be. (http://www.rmsoft.be/)
+ * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * @version 7.4.1.0
+ * @package Tiger
+ */
+class HttpRequest
+{
+    private Client $httpClient;
+    private string $baseUri;
+
+    /**
+     * HttpRequest constructor.
+     * @param string $baseUri
+     */
+    public function __construct(string $baseUri = '')
+    {
+        $this->httpClient = new Client();
+        $this->baseUri = $baseUri;
+    }
+
+    /**
+     * Perform a get HttpRequest
+     *
+     * @param string $url
+     * @param string|null $username
+     * @param string|null $password
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function get(string $url, ?string $username, ?string $password): ResponseInterface
+    {
+        return $this->httpClient->request('GET', $this->baseUri . $url, [
+            'auth' => [$username, $password]
+        ]);
+    }
+
+    /**
+     * Perform a post HttpRequest
+     *
+     * @param string $url
+     * @param string $body
+     * @param string|null $username
+     * @param string|null $password
+     * @param string $contentType
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function post(
+        string $url,
+        string $body,
+        ?string $username,
+        ?string $password,
+        string $contentType = 'application/json'
+    ): ResponseInterface {
+        return $this->httpClient->request('POST', $this->baseUri . $url, [
+            'auth' => [$username, $password],
+            'headers' => ['Content-Type' => $contentType, 'accept' => $contentType],
+            'body' => $body
+        ]);
+    }
+
+    /**
+     * Perform a put HttpRequest
+     *
+     * @param string $url
+     * @param string $body
+     * @param string|null $username
+     * @param string|null $password
+     * @param string $contentType
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function put(
+        string $url,
+        string $body,
+        ?string $username,
+        ?string $password,
+        string $contentType = 'application/json'
+    ): ResponseInterface {
+        return $this->httpClient->request('PUT', $this->baseUri . $url, [
+            'auth' => [$username, $password],
+            'headers' => ['content-type' => $contentType, 'accept' => $contentType],
+            'body' => $body
+        ]);
+    }
+
+    /**
+     * Perform a patch HttpRequest
+     *
+     * @param string $url
+     * @param string $body
+     * @param string|null $username
+     * @param string|null $password
+     * @param string $contentType
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function patch(
+        string $url,
+        string $body,
+        ?string $username,
+        ?string $password,
+        string $contentType = 'application/json'
+    ): ResponseInterface {
+        return $this->httpClient->request('PATH', $this->baseUri . $url, [
+            'auth' => [$username, $password],
+            'headers' => ['content-type' => $contentType, 'accept' => $contentType],
+            'body' => $body
+        ]);
+    }
+
+    /**
+     * Perform a delete HttpRequest
+     *
+     * @param string $url
+     * @param string|null $username
+     * @param string|null $password
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function delete(string $url, ?string $username, ?string $password): ResponseInterface
+    {
+        return $this->httpClient->request('DELETE', $this->baseUri . $url, [
+            'auth' => [$username, $password]
+        ]);
+    }
+
+    /**
+     * Retrieve the BaseUri that is being used
+     *
+     * @return string
+     */
+    public function getBaseUri(): string
+    {
+        return $this->baseUri;
+    }
+
+    /**
+     * Set the BaseUri that need to be used
+     *
+     * @param string $baseUri
+     */
+    public function setBaseUri(string $baseUri): void
+    {
+        $this->baseUri = $baseUri;
+    }
+}
